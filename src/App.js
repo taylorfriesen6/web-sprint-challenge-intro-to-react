b57f05dff9f35439b6df994b1e040c02bff9a224
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-19 12:30:47 -0800

    dealt with variation in possible API formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import Character from './components/Character.js';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,15 +13,27 @@ const App = () => {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    axios.get(`https://swapi.dev/api/people/`)
-      .then(res => setCharacters(res.data.results));
+    // const tempCharacters = [];
+    // for (let i = 1; i <= 10; i++) {
+    //   axios.get(`https://swapi.dev/api/people/${i}`)
+    //   .then(res => {
+    //     setCharacters(characters.concat([res.data]));
+    //   })
+    //   .catch(err => console.log(err));
+    // }
+    // TODO: sort characters!
+
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => setCharacters(res.data.results || res.data))
+      .catch(err => console.log(err));
   }, []);
 
   console.log(characters);
-
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <p>{characters.length}</p>
+      {characters.map(character => <Character character={character}/>)}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = ({character}) => {
+    return <p>{character.name}</p>;
+}
+
+export default Character;
